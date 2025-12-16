b792fefc1a8685840002efd62e0578ccb9ab8c6e
USHAKIRAN BUDDALA <buddalaushakiran7@gmail.com>
2025-12-16 22:33:45 +0530

    Add React frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <main style={{display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center'}}>
+      <h1 style={{fontFamily: 'system-ui, sans-serif', fontSize: '2.25rem'}}>welcome to my website</h1>
+    </main>
+  )
+}
+
+export default App
