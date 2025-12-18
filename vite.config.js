33ec7294d779d535cd08ec66572f9e65e420db03
USHAKIRAN BUDDALA <buddalaushakiran7@gmail.com>
2025-12-18 14:33:02 +0530

    Update Attendance App with PWA setup

--- vite.config.js
+++ vite.config.js
@@ -9,23 +9,15 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['icon-192.png', 'icon-512.png'],
       manifest: {
-        name: 'ATTENDENCE APP',          
-        short_name: 'ATTENDENCE APP',    
+        name: 'Attendance App',
+        short_name: 'AttendanceApp',
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#000000',
         icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
